546b7850d63d9ee1d1fe4a771ec4998847576ac9
Musa Ceylan <mcmusaceylan@gmail.com>
2016-11-09 18:55:43 +0200

    Update CrawlApplication.java

--- src/main/java/dev/CrawlApplication.java
+++ src/main/java/dev/CrawlApplication.java
@@ -6,17 +6,14 @@ import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.json.JsonJsonParser;
 import org.springframework.boot.json.JsonParser;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URI;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -27,31 +24,56 @@ public class CrawlApplication {
     public static void main(String[] args) throws IOException, URISyntaxException {
         SpringApplication.run(CrawlApplication.class, args);
 
-        List<JSONObject> articles = new ArrayList<>();
+        List<JSONObject> articles = new ArrayList();
 
 
-        String domain = "http://dergipark.gov.tr/api/public/oai/?verb=ListRecords";
+        String Link = "http://dergipark.gov.tr/api/public/oai/?verb=ListRecords";
 
 
-        while (getResumptionToken(domain) != ""){
-            processDomain(articles,domain);
-            String resumptionToken = getResumptionToken(domain);
-            domain = domain + "&resumptionToken="  + resumptionToken;
-            generateResults(articles, domain);
+        processLink(articles, Link);
+        while (checkResumptionToken(Link)) {
+            Link = updateLink(Link);
+            processLink(articles, Link);
         }
+        generateResults(articles, Link);
 
 
     }
 
 
+    public static String updateLink(String Link) {
+        String resumptionToken = getResumptionToken(Link);
+        Link = Link + "&resumptionToken=" + resumptionToken;
+        return Link;
+    }
 
-    public static String getResumptionToken(String domain) {
+    public static boolean checkResumptionToken(String Link) {
 
 
         String resumptionToken = "";
         Document doc = new Document("");
         try {
-            doc = Jsoup.connect(domain).get();
+            doc = Jsoup.connect(Link).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (doc.select("ListRecords").contains("resumptionToken")) {
+            return true;
+        } else {
+
+            return false;
+        }
+
+    }
+
+    public static String getResumptionToken(String Link) {
+
+
+        String resumptionToken = "";
+        Document doc = new Document("");
+        try {
+            doc = Jsoup.connect(Link).get();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -59,32 +81,25 @@ public class CrawlApplication {
         if (doc.select("ListRecords").contains("resumptionToken")) {
             return resumptionToken = doc.select("ListRecords").attr("resumptionToken").toString();
 
-        }else {
+        } else {
 
             return "";
         }
     }
 
 
-    private static void processDomain(List<JSONObject> articles, String domain) throws URISyntaxException {
+    private static void processLink(List<JSONObject> articles, String Link) throws URISyntaxException {
+
         Document doc = new Document("");
 
 
         try {
-            doc = Jsoup.connect(domain).get();
-            System.out.println("Connected to site - "+ domain);
-        } catch (Exception e) {
-            return System.out.println("error");
+            doc = Jsoup.connect(Link).get();
+            System.out.println("Connected to site - " + Link);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        for (Element record : doc.select("record")) {
-
-
-          if (!record.attr("identifier").isEmpty) {
-              System.out.println(record.attr("identifier"));
-          }
-
-        }
 
         // JSON LD
         for (Element JSONLD : doc.select("script[type=application/ld+json]")) {
@@ -102,104 +117,33 @@ public class CrawlApplication {
         }
 
         // For each link DOM element with RSS XML content
-        for (Element link : doc.select("link[type=application/rss+xml]")) {
+        for (Element identifier : doc.select("record")) {
 
 
-
-            JSONObject feed = new JSONObject();
-            feed.put("categoryId", feedCategoryId);
-            feed.put("description", feedDescription);
-            feed.put("id", link.attr("title").toLowerCase().replaceAll("[+.^:,|»]", "").replaceAll(" ", "-").replaceAll("--", ""));
-
-            if (feedIcon.contains("http")) {
-                feed.put("image", feedIcon);
-            } else if (feedIcon.length() > 0) {
-                URI uri = new URI(site);
-                String domain = uri.getHost();
-                feed.put("image", domain + feedIcon);
-            }
+            JSONObject article = new JSONObject();
+            article.put("id", identifier);
 
 
-            try {
-                HttpURLConnection.setFollowRedirects(true);
-                // note : you may also need
-                //        HttpURLConnection.setInstanceFollowRedirects(false)
-                HttpURLConnection con =
-                        (HttpURLConnection) new URL(feed.get("image").toString()).openConnection();
-                con.setRequestMethod("HEAD");
-                con.connect();
-
-                if(con.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                    feed.put("image", "http://www.crownmountainmovers.com/wp-content/uploads/2015/10/arrow-right1.png");
-                }
-            }
-            catch (Exception e) {
-                feed.put("image", "http://www.crownmountainmovers.com/wp-content/uploads/2015/10/arrow-right1.png");
-            }
-
-            if (link.attr("title").contains("rss") || link.attr("title").contains("RSS")) {
-                Elements title = doc.select("title");
-                feed.put("title", title.text());
-            } else if (link.attr("title").length() > 0) {
-                feed.put("title", link.attr("title"));
-            } else {
-                feed.put("title", "");
-            }
-
-
-            if (link.attr("href").contains("http")) {
-                feed.put("url", link.attr("href"));
-            } else if (link.attr("href").length() > 0) {
-                feed.put("url", site + link.attr("href"));
-            }
-
-
-            Iterator<?> keys = feed.keys();
-
-            while (keys.hasNext()) {
-                String key = (String) keys.next();
-                if ((feed.get(key) == null || feed.get(key) == "") && !(key.equals("description"))) {
-                    dataIncomplete = true;
-                }
-            }
-
-            if (dataIncomplete) {
-                sitesInCompleteData.add(feed);
-            } else {
-                feeds.add(feed);
-            }
-
-            System.out.println("RSS Feed: " + link.attr("title") + ", Content: " + link.attr("href"));
         }
-    }
-  }
 
-    private static void generateResults(List<JSONObject> articles,  String domain) throws IOException {
+    }
+
+    private static void generateResults(List<JSONObject> articles, String Link) throws IOException {
 
         System.out.println("\n\nWriting news feeds to feeds.json\n");
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         JsonParser jp = new JsonJsonParser();
 
-        Object je = jp.parseList(feeds.toString());
+        Object je = jp.parseList(articles.toString());
         String prettyJsonString = gson.toJson(je);
 
-        PrintWriter writer = new PrintWriter("feeds.json", "UTF-8");
+        PrintWriter writer = new PrintWriter("articles.json", "UTF-8");
         writer.print(prettyJsonString);
         writer.close();
 
-
-        System.out.println("Writing incomplete data to incomplete.json");
-        je = jp.parseList(sitesInCompleteData.toString());
-        prettyJsonString = gson.toJson(je);
-
-
-        writer = new PrintWriter("incomplete.json", "UTF-8");
-        writer.print(prettyJsonString);
-        writer.close();
-
-        //Close the input stream
-        br.close();
 
         System.out.println("Success! Processing complete.");
     }
+
+}
